837e871fb3cbf8eb74a4a770858b0fd9905a0d74
Anas Rchid <archid-@student.1337.ma>
2019-10-11 00:42:59 +0100

    working on #3: documenting on sort algorithms and their time complexity

--- include/checker.h
+++ include/checker.h
@@ -1,13 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/09 11:32:28 by archid-           #+#    #+#             */
+/*   Created: 2019/10/09 11:33:10 by archid-           #+#    #+#             */
 /*   Updated: 2019/10/09 11:49:03 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "stack.h"
+
+t_ps	read_stack(int argc, char **argv)
+{
+	t_ps	stack;
+	t_s64	val;
+	int		i;
+
+	i = 1;
+	stack = NULL;
+	while (i < argc)
+	{
+		val = ft_atoll(argv[i++]);
+		if (val > INT_VALMAX || val < INT_VALMIN)
+		{
+			ps_del(&stack);
+			return (NULL);
+		}
+		ps_push(stack, (t_val)val);
+	}
+	return (stack);
+}
+
+/* FIXME: this is should be done using quick-sort */
+void	sort_asc_stack(t_ps stack)
+{
+	(void)stack;
+}
+
+/* FIXME: this waits for stdin */
+void	read_ops(t_ps stack)
+{
+
+}
+
+#endif
--- src/push_swap/refactor.c
+++ src/push_swap/refactor.c
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   refactor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/09 11:32:28 by archid-           #+#    #+#             */
-/*   Updated: 2019/10/09 11:49:03 by archid-          ###   ########.fr       */
+/*   Created: 2019/10/09 23:06:41 by archid-           #+#    #+#             */
+/*   Updated: 2019/10/09 23:21:36 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#include "stack.h"
+
+/* NOTE: those mids 1 and 2 are just the index of elements n/3 and (2*n)/3 */
+void	split_by_mids(t_ps stack, t_u32 mid1, t_u32 mid2)
+{
+
+}
+
+void	ps_asc_quicksort(t_ps stack)
+{
+
+}
